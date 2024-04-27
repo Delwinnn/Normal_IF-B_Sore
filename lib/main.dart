3bc1e4604701dad67595305ae28ce88cc678b4cd
Delwinnn <221110202@students.mikroskil.ac.id>
2024-04-27 22:13:53 +0700

    nenambahkan provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:agile_git/LoginPage.dart';
-import 'package:agile_git/StockPage.dart';
+import 'package:agile_git/provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,9 +12,14 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: LoginView(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => ProviderGudang(),)
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: LoginView(),
+      ),
     );
   }
 }
