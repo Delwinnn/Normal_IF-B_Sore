30c5bec9c2b020c157d94d2cec49c5772490e636
Delwinnn <221110202@students.mikroskil.ac.id>
2024-05-24 22:31:49 +0700

    menambah fitur imagepicker

--- lib/AddStockPage.dart
+++ lib/AddStockPage.dart
@@ -1,7 +1,15 @@
+import 'dart:io';
+
 import 'package:agile_git/provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
+
+XFile? _image = null;
+final ImagePicker _picker = ImagePicker();
+int? valimg = 0;
 
 class AddStock extends StatefulWidget {
   const AddStock({super.key});
@@ -17,6 +25,12 @@ class _AddStockState extends State<AddStock> {
   TextEditingController stock = TextEditingController();
   int? valstock = 1;
   
+  @override
+  void dispose() {
+    super.dispose();
+    _image = null;
+    valimg = 0;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +42,7 @@ class _AddStockState extends State<AddStock> {
       ),
       body: SingleChildScrollView(
         child: Container(
-          padding: EdgeInsets.all(20),
+          padding: EdgeInsets.symmetric(horizontal: 20,vertical: 15),
           width: MediaQuery.of(context).size.width,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -39,25 +53,141 @@ class _AddStockState extends State<AddStock> {
                   Text("Add New Product", style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),)
                 ]
               ),
-              SizedBox(height: 20,),
-              Text("Link Image : ",style: TextStyle(fontSize: 18),),
-              SizedBox(height: 10,),
-              TextField(
-                maxLines: null,
-                controller: link,
-                decoration: InputDecoration(
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black),
-                    borderRadius: BorderRadius.circular(15)
+              SizedBox(height: 13,),
+              if(_image != null)
+              Center(
+                child: Image(
+                  image: FileImage(File(_image!.path)),
+                  height: 200,
+                  width: 200,
+                  fit: BoxFit.contain,
+                ),
+              )
+              else if(link.text != "")
+              Center(
+                child: Image(
+                  image: NetworkImage(link.text),
+                  height: 200,
+                  width: 200,
+                ),
+              ),
+              if(valimg == 1)
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      maxLines: null,
+                      controller: link,
+                      decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black), 
+                          borderRadius: BorderRadius.circular(15)
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blue),
+                          borderRadius: BorderRadius.circular(15)
+                        ),
+                        fillColor: Colors.white,
+                        filled: true,
+                        hintText: "Image URL",
+                        hintStyle: TextStyle(fontSize: 16)
+                      ),
+                    ),
                   ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.blue),
-                    borderRadius: BorderRadius.circular(15)
+                  SizedBox(width: 10,),
+                  IconButton(
+                    onPressed: () {
+                      setState(() {
+                      });
+                    }, 
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStatePropertyAll(Colors.blue)
+                    ),
+                    icon: Icon(Icons.done,color: Colors.white,size: 35)
                   ),
-                  fillColor: Colors.white,
-                  filled: true,
-                  hintText: "https://.........",
-                  hintStyle: TextStyle(fontSize: 16)
+                ]
+              ),
+              SizedBox(height: 15,),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    showModalBottomSheet<void>(
+                      isScrollControlled: true,
+                      context: context,
+                      builder: (context) => DraggableScrollableSheet(
+                        expand: false,
+                        initialChildSize: 0.6,
+                        builder: (context, scrollController) => SingleChildScrollView(
+                          controller: scrollController,
+                          child: Center(
+                            child: Container(
+                              padding: EdgeInsets.all(20),
+                              child: Column(
+                                children: [
+                                  Row(
+                                    children: [Text("Product Image",style: TextStyle(fontSize: 15),)]
+                                  ),
+                                  SizedBox(height: 15,),
+                                  ListTile(
+                                    onTap:() async {
+                                      Navigator.pop(context);
+                                      valimg = 0;
+                                      final XFile? image = await _picker.pickImage(source: ImageSource.camera);
+                                      setState(() {
+                                        _image = image;
+                                      });
+                                    },
+                                    leading: Icon(Icons.camera_alt_outlined,size: 30,),
+                                    title: Text('Capture from Camera',style: TextStyle(fontSize: 18),),
+                                  ),
+                                  ListTile(
+                                    onTap:() async {
+                                      Navigator.pop(context);
+                                      valimg = 0;
+                                      final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+                                      setState(() {
+                                        _image = image;
+                                      });
+                                    },
+                                    leading: Icon(Icons.photo_library_outlined,size: 30,),
+                                    title: Text('Choose from Gallery',style: TextStyle(fontSize: 18),),
+                                  ),
+                                  ListTile(
+                                    onTap:() {
+                                      Navigator.pop(context);
+                                      setState(() {
+                                        valimg=1;
+                                      });
+                                    },
+                                    leading: Icon(Icons.link_outlined,size: 30,),
+                                    title: Text('Input Link URL',style: TextStyle(fontSize: 18),),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: valimg==0 && _image==null ? EdgeInsets.all(30) : EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+                    backgroundColor: Colors.grey[300],
+                    foregroundColor: Colors.grey[800],
+                    shape: RoundedRectangleBorder(
+                      borderRadius: valimg==0 && _image==null ? BorderRadius.circular(15) : BorderRadius.circular(30)
+                    ),
+                    elevation: 5,
+                  ),
+                  child: valimg==0 && _image==null 
+                  ? Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.add_photo_alternate_rounded,size: 60,),
+                      Text("Add Image",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 17),)
+                    ],
+                  )
+                  : Text("Change",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 17,color: Colors.blue))
                 ),
               ),
               SizedBox(height: 20,),
@@ -155,8 +285,10 @@ class _AddStockState extends State<AddStock> {
                   ElevatedButton.icon(
                     onPressed: () {
                       setState(() {
-                        List item = [link.text, name.text, brand.text, valstock, stock.text];
-                        Provider.of<ProviderGudang>(context,listen: false).addStock(context, item);
+                        List item = valimg==0 
+                        ? [_image, name.text, brand.text, valstock, stock.text] 
+                        : [link.text, name.text, brand.text, valstock, stock.text];
+                        Provider.of<ProviderGudang>(context,listen: false).addStock(context, item, valimg);
                       });
                     }, 
                     style: ElevatedButton.styleFrom(
@@ -195,3 +327,4 @@ class _AddStockState extends State<AddStock> {
     );
   }
 }
+
