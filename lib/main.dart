5afe0b5a0788d949a8ffa349056cda0dc49406c3
Richard <221111467@students.mikroskil.ac.id>
2024-06-08 22:37:00 +0700

    Sekarang saat pertama membuka aplikasi, user akan diarahkan ke Landing Page terlebih dahulu

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:agile_git/LoginPage.dart';
+import 'package:agile_git/LandingPage.dart';
 import 'package:agile_git/provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -18,8 +18,53 @@ class MainApp extends StatelessWidget {
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: LoginView(),
+        home: SplashScreenPage(),
       ),
     );
   }
 }
+
+
+
+class SplashScreenPage extends StatefulWidget {
+  @override
+  _SplashScreenPageState createState() => _SplashScreenPageState();
+}
+
+class _SplashScreenPageState extends State<SplashScreenPage> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToHome();
+  }
+
+  _navigateToHome() async {
+    await Future.delayed(Duration(seconds: 2), () {}); 
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => Landing()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen();
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:[
+           Image.asset('assets/stockify.png'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
